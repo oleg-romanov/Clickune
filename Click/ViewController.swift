1b43f5253764e34f605698b871c5687fa88ba9bc
Oleg Romanov <Romanov.3003@mail.ru>
2020-08-01 12:15:08 +0300

    Initial Commit

---
+++ Click/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Click
+//
+//  Created by Олег Романов on 01.08.2020.
+//  Copyright © 2020 Oleg Romanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
